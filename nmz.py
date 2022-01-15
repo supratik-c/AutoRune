f70e8feb74ae4a9961cd77ec170af338b08a50aa
supratik-c <scdipto@gmail.com>
2022-01-15 14:19:12 +0000

    NMZ updated

--- nmz.py
+++ nmz.py
@@ -7,7 +7,14 @@ import time
 
 for i in range(10000):
     ck.move(2266, 167)
-    pg.leftClick()
+    pg.doubleClick(interval=0.6)
     time.sleep(0.1)
-    pg.leftClick()
-    time.sleep(50)
+    
+    if i % 20 == 0:
+        for row in range(2, 8):
+            for col in range(1, 5):
+                inv_x, inv_y = ck.inventory(col, row)
+                ck.move(inv_x, inv_y, speed = 0.1)
+                pg.leftClick()
+    
+    time.sleep(30)
